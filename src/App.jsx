f84c080aa1de6022b4af539c9dcd06db3f97c1d1
Qasim <qalhamad33@gmail.com>
2024-08-11 14:32:50 +0300

    Navbar and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import { useState } from "react";
+import Navbar from "./components/NavBar";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
-// src/App.jsx
-
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main>
+              <h1>Post Office</h1>
+            </main>
+          }
+        />
+        <Route path="/mailboxes" element={<MailboxList />} />
+        <Route path="/new-mailbox" element={<MailboxList />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
